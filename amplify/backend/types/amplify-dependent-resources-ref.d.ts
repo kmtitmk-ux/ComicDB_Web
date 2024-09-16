e3d859b0ee3cbd68481537e43952b00dfed7b2ca
Kazuki Matsui <kmtitmk123@gmail.com>
2024-09-16 12:54:36 +0900

    no message

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "comicdb": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
